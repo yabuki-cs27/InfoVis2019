1a0c70a858e230a3d5324830af3054e333033413
矢吹　直也 <192x221x@istc-c-025.epc.center.kobe-u.ac.jp>
2019-04-23 16:28:17 +0900

    add ex5_2

--- W05/main2.js
+++ W05/main2.js
@@ -58,18 +58,35 @@ function main()
       geometry.faces.push( f[i] );
     }
 
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    var material = new THREE.MeshBasicMaterial( { color: 0xffffff ,vertexColors:THREE.FaceColors} );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
-    document.addEventListener('mousedown',mouse_down_event);
+    document.addEventListener('click',mouse_down_event);
     function mouse_down_event(event){
-      var raycaster = new THREE.Raycaster( origin, direction );
-      var intersects = raycaster.intersectObject( triangle );
+      var rect = event.target.getBoundingClientRect();
+      var x_win = event.clientX;
+      var y_win = event.clientY;
+
+      var vx = renderer.domElement.offsetLeft;
+      var vy = renderer.domElement.offsetTop;
+      var vw = renderer.domElement.width;
+      var vh = renderer.domElement.height;
+      var x_NDC = 2 * ( x_win - vx ) / vw - 1;
+      var y_NDC = -( 2 * ( y_win - vy ) / vh - 1 );
+
+      var pos = new THREE.Vector3(x_NDC,y_NDC,1);
+
+      pos.unproject(camera);
+
+      var raycaster = new THREE.Raycaster( camera.position, pos.sub(camera.position).normalize() );
+      var intersects = raycaster.intersectObjects( scene.children);
       if ( intersects.length > 0 )
       {
-        intersects[0].face.color.setRGB( 1, 0, 0 );
+        intersects[0].face.color.set(0xff0000);
         intersects[0].object.geometry.colorsNeedUpdate = true;
+        intersects[0].object.geometry.elementNeedUpdate = true;
+        console.log(intersects[0].face);
       }
     }
 
